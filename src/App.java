99a5e4b9dd3e3d681197502b04e343410161aff9
Ederson Davi <EdersonDav@MBP-de-Rafaela.Home>
2025-01-25 18:20:00 +0000

    fix: search number and overflow

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         BinarySearch binarySearch = new BinarySearch();
         int[] arrayNumbers = { 1, 3, 5, 7, 9 };
-        System.out.println(binarySearch.search(arrayNumbers, 3, 0, arrayNumbers.length - 1));
+        System.out.println(binarySearch.search(arrayNumbers, 12, 0, arrayNumbers.length - 1));
     }
 }
--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,14 +1,16 @@
 
 public class BinarySearch {
     public int search(int[] arrayNumber, int numberToSearch, int smallest, int largest) {
-        int mid = (int) Math.floor((smallest + largest) / 2);
+        if (smallest > largest)
+            return -1;
+        int mid = smallest + (largest - smallest) / 2;
         if (numberToSearch == arrayNumber[mid]) {
             return mid;
         }
         if (numberToSearch > arrayNumber[mid]) {
-            return search(arrayNumber, numberToSearch, mid, largest);
+            return search(arrayNumber, numberToSearch, mid + 1, largest);
         } else if (numberToSearch < arrayNumber[mid]) {
-            return search(arrayNumber, numberToSearch, smallest, mid);
+            return search(arrayNumber, numberToSearch, smallest, mid - 1);
         }
         return -1;
     }
