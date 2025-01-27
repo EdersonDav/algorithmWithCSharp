db27bc0e58a0d9ba2796b1f53b55a7017ada2f6a
Ederson Davi <EdersonDav@MBP-de-Rafaela.Home>
2025-01-27 21:37:06 +0000

    refactory: change to C#

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        BinarySearch binarySearch = new BinarySearch();
-        int[] arrayNumbers = { 1, 3, 5, 7, 9 };
-        System.out.println(binarySearch.search(arrayNumbers, 12, 0, arrayNumbers.length - 1));
-    }
-}
